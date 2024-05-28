c2d04ce9c1811dc7bf08badd66ed3764340d9372
Riadsantir <112951270+Riadsantir@users.noreply.github.com>
2024-05-28 02:38:01 +0200

    Initial commit

---
+++ subtheme/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.fox = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
